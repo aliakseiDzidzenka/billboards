f5d1d5e2fa9c0defd40b0dde7bf1209cdb7cd391
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2018-12-07 22:27:07 +0300

    log in, sign up buttons at menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
+  get  '/signup',  to: 'users#new'
+
   root 'welcome#index'
 end
