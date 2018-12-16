6c87ce41d8e8eb5172375f979ed125c2184bb952
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2018-12-17 02:00:34 +0300

    devise + registration + confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  get  '/signup',  to: 'users#new'
+  #resources :users
+  #get  '/signup',  to: 'users#new'
 
   root 'welcome#index'
 end
