2e31cb898aae429a609c4d561fec6284dfd08baa
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2018-12-18 13:45:17 +0300

    time zone now confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_234847) do
+ActiveRecord::Schema.define(version: 2018_12_18_104000) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
